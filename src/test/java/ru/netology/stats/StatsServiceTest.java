feb7917972fde99db8189210e056a79ce4011a62
iva1111 <mvg75@mail.ru>
2021-08-05 12:46:59 +0500

    simplification test, add jacoco

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -4,71 +4,87 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class StatsServiceTest {
-
+class StatsServiceTest {
     long[] monthSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+    StatsService service = new StatsService();
 
-    @Test
-    public void sumSale() {    // сумма всех продаж, задача 1
-        StatsService service = new StatsService();
-
+    //@Test
+      /* public void sumSale() {    // сумма всех продаж, задача 1
         long expected = 180;
         long actual = service.sumSale(monthSales);
-
         assertEquals(expected, actual);
+    }*/
+
+    // упрoщение, укорочение теста
+    @Test
+    public void sumSale() {    // сумма всех продаж, задача 1
+        assertEquals(180, service.sumSale(monthSales));
     }
 
-    @Test
+   /* @Test
     public void averageMonthlySales() {   // Средняя сумма продаж в месяц, задача 2
-        StatsService service = new StatsService();
-
         long expected = 15;
         long actual = service.averageMonthlySales(monthSales);
-
         assertEquals(expected, actual);
+    }*/
+
+    // упрoщение, укорочение теста
+    @Test
+    public void averageMonthlySales() {   // Средняя сумма продаж в месяц, задача 2
+        assertEquals(15, service.averageMonthlySales(monthSales));
     }
 
-    @Test
+   /* @Test
     public void maxSales() {    // номер месяца max продаж, задача 3
-        StatsService service = new StatsService();
-
         long expected = 8;
         long actual = service.maxSales(monthSales);
-
         assertEquals(expected, actual);
+    }*/
+
+    // упрoщение, укорочение теста
+    @Test
+    public void maxSales() {    // номер месяца max продаж, задача 3
+        assertEquals(8, service.maxSales(monthSales));
     }
 
-
-    @Test
+   /* @Test
     public void minSales() {    // номер месяца min продаж, задача 4
-        StatsService service = new StatsService();
-
         long expected = 9;
         long actual = service.minSales(monthSales);
-
         assertEquals(expected, actual);
+    }*/
+
+    // упрoщение, укорочение теста
+    @Test
+    public void minSales() {    // номер месяца min продаж, задача 4
+        assertEquals(9, service.minSales(monthSales));
     }
 
-    @Test
+   /* @Test
     public void numberMonthsLowSales() {    // Количество месяцев продажи ниже среднего, задача 5
-        StatsService service = new StatsService();
-
         long expected = 5;
         long actual = service.numberMonthsLowSales(monthSales);
-
         assertEquals(expected, actual);
+    }*/
+
+    // упрoщение, укорочение теста
+    @Test
+    public void numberMonthsLowSales() {    // Количество месяцев продажи ниже среднего, задача 5
+        assertEquals(5, service.numberMonthsLowSales(monthSales));
     }
 
-    @Test
+    /*@Test
     public void numberMonthsHidhSales() {    // Количество месяцев продажи выше среднего, задача 6
-        StatsService service = new StatsService();
-
         long expected = 5;
         long actual = service.numberMonthsHidhSales(monthSales);
-
         assertEquals(expected, actual);
-    }
+    }*/
 
+    // упрoщение, укорочение теста
+    @Test
+    public void numberMonthsHidhSales() {    // Количество месяцев продажи выше среднего, задача 6
+        assertEquals(5, service.numberMonthsHidhSales(monthSales));
+    }
 }
 
 
